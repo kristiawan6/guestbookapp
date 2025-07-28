abeeaf8760c691666290580224e3a20bde15a2b5
William Kristiawan <macbook@192.168.1.118>
2025-07-28 19:19:55 +0700

    fix scanner

--- app/admin/claim-souvenir/[itemId]/page.tsx
+++ app/admin/claim-souvenir/[itemId]/page.tsx
@@ -115,6 +115,7 @@ export default function ClaimDetail({
           timer: 1500,
         });
 
+        setIsScannerOpen(false);
         await fetchAllData();
       } catch (error: unknown) {
         const message =
